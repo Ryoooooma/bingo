f6ef873bf2fa8c6f73b5105d97c5c8192878c8ec
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-16 16:51:55 +0800

    developed logic of all lines in Bingo

--- index.php
+++ index.php
@@ -8,16 +8,26 @@
 // G: 46 - 60
 // O: 61 - 75
 
-// range関数は1から15までの数の配列を作る関数
-$numbers = range(1, 15);
-// ()内のオブジェクトをシャッフルする関数
-shuffle($numbers);
-// 配列の添え字0から5つ分のデータをとってくる関数
-$rs = array_slice($numbers, 0, 5);
-// ()内のオブジェクトの中身とデータ型をはき出す
-var_dump($rs);
+// // range関数は1から15までの数の配列を作る関数
+// $numbers = range(1, 15);
+// // ()内のオブジェクトをシャッフルする関数
+// shuffle($numbers);
+// // 配列の添え字0から5つ分のデータをとってくる関数
+// $rs = array_slice($numbers, 0, 5);
+// // ()内のオブジェクトの中身とデータ型をはき出す
+// var_dump($rs);
 
+$bingo = array();
 
+for ($i=0; $i<5; $i++) {
+	$numbers = range($i*15+1, $i*15+15);
+	// ()内のオブジェクトをシャッフルする関数
+	shuffle($numbers);
+	// 配列の添え字0から5つ分のデータをとってくる関数
+	$bingo[$i] = array_slice($numbers, 0, 5);
+}
+
+var_dump($bingo);
 
 
 
